83fd95e9a5c86d69bee6d152cfc6f52ace1f62dc
Madhava <madhavayedla@gmail.com>
2023-02-07 19:26:20 +0530

    modified javascript

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
-// let slideIndex = 0;
-// showSlides();
-
-// function showSlides() {
-//   let i;
-//   let slides = document.getElementsByClassName("slide");
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   slideIndex++;
-//   if (slideIndex > slides.length) {slideIndex = 1}
-//   slides[slideIndex-1].style.display = "block";
-//   setTimeout(showSlides, 2000);
-// } 
 
 
 
-function div(){
-  var div = $('.container'),
-      now = div.filter(':visible'),
-      next = now.next().length ? now.next() : div.first(),
-      speed = 1000;
+// function div(){
+//   var div = $('.container'),
+//       now = div.filter(':visible'),
+//       next = now.next().length ? now.next() : div.first(),
+//       speed = 1000;
+// console.log(div);
 
-  
-  now.fadeOut(speed);
-  now.fadeIn(speed);
+//   now.fadeOut(speed);
+//   now.fadeIn(speed);
+// }
+
+
+// $(function(){
+//   setInterval(div,1400);
+// })
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 2000); 
 }
-
-
-$(function(){
-  setInterval(div,1400);
-})
