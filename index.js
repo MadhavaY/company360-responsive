1568d02750dae43d552b95a85dc7b672187f8d4a
Madhava <madhavayedla@gmail.com>
2023-02-07 19:11:14 +0530

    added javascript

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
-let slideIndex = 0;
-showSlides();
+// let slideIndex = 0;
+// showSlides();
 
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("slide");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000);
-} 
+// function showSlides() {
+//   let i;
+//   let slides = document.getElementsByClassName("slide");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1}
+//   slides[slideIndex-1].style.display = "block";
+//   setTimeout(showSlides, 2000);
+// } 
+
+
+
+function div(){
+  var div = $('.container'),
+      now = div.filter(':visible'),
+      next = now.next().length ? now.next() : div.first(),
+      speed = 1000;
+
+  
+  now.fadeOut(speed);
+  now.fadeIn(speed);
+}
+
+
+$(function(){
+  setInterval(div,1400);
+})
